a066c47a611e2ae72c3f70e309e004c68ae8a604
Salifu Yakubu <salifuyakubu333@gmail.com>
2026-07-10 18:32:07 +0000

    making submission 8 more robust.

--- submission/08.sh
+++ submission/08.sh
@@ -1,4 +1,6 @@
 # What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
 transaction="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
-bitcoin-cli -regtest decodepsbt "$transaction" | jq -r '.tx.vout[0].scriptPubKey.address // .tx.vout[0].scriptPubKey.addresses[0]'
+
+bitcoin-cli -regtest decodepsbt "$transaction" | \
+  jq -r '.tx.vout[] | select(.scriptPubKey.address != null) | .scriptPubKey.address'
